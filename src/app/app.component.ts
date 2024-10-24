ecd503316c9ab929098577e5cd5e27658bae4e9f
AYRTONFLORIAN30 <ayrton.florian@tecsup.edu.pe>
2024-10-24 16:28:05 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directivas-angular';
+  dia: string = '';  
+  title = 'apli101';
+
+  personas = [
+    { nombre: 'Ricardo', edad: 40 },
+    { nombre: 'Salvador', edad: 20 }
+  ];
 }
